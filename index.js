dabe140c846c96925e6227548f4098357e4cdc2a
aremumahmud <74737123+aremumahmud@users.noreply.github.com>
2021-12-28 11:11:54 +0100

    bj

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 var express = require("express")
 var app = express()
+var os = require("os")
 app.listen(process.env.PORT || 2000)
 
 app.get("/" ,(req,res)=>{
-res.send("i am working "+process.pid)
+res.send("i am working "+process.pid+".  "+ os.cpus().length)
 })
